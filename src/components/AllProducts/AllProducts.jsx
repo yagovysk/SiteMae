543668f88d892f19b9115418d334f5434d3441f2
yagovysk <yago09regis@gmail.com>
2024-05-29 22:20:17 -0300

    add

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -3,7 +3,7 @@ import { MenuClick } from "../Menu/MenuClick";
 import { Title } from "./Components-All/Title/Title";
 import { Allintro } from "./Components-All/Allintro/Allintro";
 import { End } from "../End/End";
-import "./AllProducts.css";
+import "./Products.css";
 
 export function AllProducts() {
   return (
