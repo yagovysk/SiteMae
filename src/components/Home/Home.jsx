8fe268640b099212ee472a12e8f1492a90633676
yagovysk <yago09regis@gmail.com>
2024-05-29 22:02:28 -0300

    add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Intro } from "./components-home/Intro/Intro";
 import { History } from "./components-home/History/History";
 import { Products } from "./components-home/Products/Products";
 import { SpecialCake } from "./components-home/SpecialCake/Special";
-import { Services } from "./components-home/Services/services";
+import { Services } from "./components-home/Services/Services";
 import { Other } from "./components-home/OtherProducts/Other";
 import { Depoiments } from "./components-home/Depoiments/Depoiments";
 import { Siga } from "./components-home/Siga/Siga";
