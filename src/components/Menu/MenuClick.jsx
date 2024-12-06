a4df73d53985677d950da34f5f6b2ff2864503bc
yagovysk <yago09regis@gmail.com>
2024-12-06 16:34:00 -0300

    tornando responsivo a parte de nossos produtos

--- src/components/Menu/MenuClick.jsx
+++ src/components/Menu/MenuClick.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./MenuClick.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
